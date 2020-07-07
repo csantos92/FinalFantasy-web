3a2a78164ea63427093d474053a4c80478b6cb18
Carlos Santos <50203409+csantos92@users.noreply.github.com>
2020-07-07 13:21:01 +0200

    Update music.js

--- js/music.js
+++ js/music.js
@@ -4,17 +4,17 @@ window.addEventListener('load', () => {
     
     var music = document.querySelector("#music");
     var audio = new Audio();
-    audio.src="../../../media/audio/prelude.mp3";
+    audio.src="media/audio/prelude.mp3";
     var mute = false;
 
     //Play or stop music
     music.addEventListener('click', () => {
         if(!mute){
-            music.src = "../../../images/icons/volume.png";
+            music.src = "images/icons/volume.png";
             audio.play();
             mute = true;
         }else{
-            music.src = "../../../images/icons/mute.png";
+            music.src = "images/icons/mute.png";
             audio.pause();
             mute = false;
         }
