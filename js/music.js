4c0633f405ece818e7bf3f8bf6231d93ea015da0
Carlos Santos <50203409+csantos92@users.noreply.github.com>
2020-07-10 11:21:17 +0200

    Update music.js

--- js/music.js
+++ js/music.js
@@ -8,17 +8,17 @@ window.addEventListener('load', () => {
     var mute = false;
     var volume_img, mute_img;
     
-    if(url.startsWith("/index")){
+    if(url.startsWith("index")){
         audio.src   = "media/audio/prelude.mp3";
         volume_img  = "images/icons/volume.png"
         mute_img    = "images/icons/mute.png"
     }
-    else if(url.startsWith("/noticias/n")){
+    else if(url.startsWith("noticias/n")){
         audio.src   = "../../media/audio/prelude.mp3";
         volume_img  = "../../images/icons/volume.png";
         mute_img    = "../../images/icons/mute.png";
     }
-    else if(url.startsWith("/noticias/2")){
+    else if(url.startsWith("noticias/2")){
         audio.src   = "../../../media/audio/prelude.mp3";
         volume_img  = "../../../images/icons/volume.png";
         mute_img    = "../../../images/icons/mute.png";
